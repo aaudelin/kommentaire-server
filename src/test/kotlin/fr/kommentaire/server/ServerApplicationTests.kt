b0b3f1b0c4856446efa00890a22a73308e942b8b
aaudelin-axione <aurelien.audelin@kaibee.fr>
2020-06-07 16:19:05 +0200

    Init project

---
+++ src/test/kotlin/fr/kommentaire/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.kommentaire.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
