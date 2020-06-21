504eb8a8d1c20f02611f34b648932d3d4515c97d
aaudelin <aurelien.audelin@gmail.com>
2020-06-21 14:34:37 +0200

    Add postgre dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.security:spring-security-test")
 	implementation("org.springframework.security:spring-security-crypto")
+	implementation("org.postgresql:postgresql")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("com.h2database:h2")
--- src/main/kotlin/fr/kommentaire/server/ServerApplication.kt
+++ src/main/kotlin/fr/kommentaire/server/ServerApplication.kt
@@ -1,11 +1,11 @@
 package fr.kommentaire.server
 
 import com.expediagroup.graphql.directives.KotlinDirectiveWiringFactory
-import fr.kommentaire.server.authentication.AuthenticationDirective
-import fr.kommentaire.server.authentication.AuthenticationSchemaDirectiveWiring
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import fr.kommentaire.server.directives.CustomDirectiveWiringFactory
 import fr.kommentaire.server.directives.CustomSchemaGeneratorHooks
-import graphql.schema.idl.RuntimeWiring
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -14,6 +14,7 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import javax.sql.DataSource
 
 
 @SpringBootApplication
@@ -47,5 +48,17 @@ class Configuration {
 	@Bean
 	fun hooks(wiringFactory: KotlinDirectiveWiringFactory) = CustomSchemaGeneratorHooks(wiringFactory)
 
+}
 
+@Configuration
+class DatabaseConfig {
+	@Value("\${spring.datasource.url}")
+	private val dbUrl: String? = null
+
+	@Bean
+	fun dataSource(): DataSource {
+		val config = HikariConfig()
+		config.jdbcUrl = dbUrl
+		return HikariDataSource(config)
+	}
 }
